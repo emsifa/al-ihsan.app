6e2b970273f6871e0db210ab644b00a2d02b0213
Muhammad Syifa <emsifa@gmail.com>
2021-03-13 08:13:31 +0700

    sementara footer pakai nama sendiri

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React, { FC } from "react";
 const Footer: FC = () => {
   return (
     <footer className="select-none mt-5 shadow-lg bg-primary rounded px-3 py-2 text-xs text-white text-center">
-      &copy; 2021 &middot; Majelis Ta'lim Al-Ihsan, Duren Sawit, Jakarta Timur
+      &copy; 2021 &middot; Muhammad Syifa
     </footer>
   );
 };
